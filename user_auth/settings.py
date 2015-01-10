621856cc4a70b81c8d624413ec0b2d065da0ac8e
mejiro <m.sh.nwpct1@gmail.com>
2015-01-10 18:27:44 +0900

    Add a new app named accounts

--- user_auth/settings.py
+++ user_auth/settings.py
@@ -66,9 +66,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
