dfacee5c812751708ce4c40e88f748181cdb7b2d
mejiro <m.sh.nwpct1@gmail.com>
2015-01-10 23:02:52 +0900

    Implement a task model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from user_auth import settings
 
-# Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='tasks')
+    summary = models.CharField('タスク', max_length=128)
+    complete = models.BooleanField('状態', default=False)
+    comment = models.CharField('コメント', max_length=512, blank=True)
+    done_date = models.DateField('完了日', null=True, blank=True)
+
+    def __str__(self):
+        return str(self.id) + ': ' + self.summary
