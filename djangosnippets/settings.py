aa311207527195a0b6148cc5485d121e4f591555
c-bata <contact@c-bata.link>
2018-05-18 21:24:50 +0900

    Add socials app and define model

--- djangosnippets/settings.py
+++ djangosnippets/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'snippets.apps.SnippetsConfig',
     'accounts.apps.AccountsConfig',
+    'socials.apps.SocialsConfig',
 ]
 
 MIDDLEWARE = [
