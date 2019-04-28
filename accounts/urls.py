fe8d900115c3aa951f4fcfc739b7f00c40e273fe
c-bata <contact@c-bata.link>
2019-04-29 01:56:55 +0900

    Refactor signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,7 +9,7 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
+    path('signup/', views.SignUp.as_view(), name='signup'),
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,27 @@
+from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic.edit import CreateView
 
 
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('top')
-    else:
-        form = UserCreationForm()
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    template_name = "accounts/signup.html"
 
-    return render(request, 'accounts/signup.html', {'form': form})
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        messages.add_message(self.request, messages.SUCCESS,
+                             "ユーザー登録に成功しました。")
+        self.object = user
+        return HttpResponseRedirect(self.get_success_url())
+
+    def form_invalid(self, form):
+        messages.add_message(self.request, messages.ERROR,
+                             "ユーザー登録に失敗しました。")
+        return super().form_invalid(form)
+
+    def get_success_url(self):
+        return reverse('top')
