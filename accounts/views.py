0028289d3b8c4abc70849f114649190c879d5f56
c-bata <contact@c-bata.link>
2019-04-29 05:19:39 +0900

    Refactor Signup view

--- accounts/views.py
+++ accounts/views.py
@@ -2,13 +2,14 @@ from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
 
 class SignUp(CreateView):
     form_class = UserCreationForm
     template_name = "accounts/signup.html"
+    success_url = reverse_lazy('top')
 
     def form_valid(self, form):
         user = form.save()
@@ -22,6 +23,3 @@ class SignUp(CreateView):
         messages.add_message(self.request, messages.ERROR,
                              "ユーザー登録に失敗しました。")
         return super().form_invalid(form)
-
-    def get_success_url(self):
-        return reverse('top')
